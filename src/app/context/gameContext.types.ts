459bd5ed3d588633ad4e4d148c236d38065167ba
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-25 14:11:23 +0300

    create game context functions

--- src/app/context/gameContext.types.ts
+++ src/app/context/gameContext.types.ts
@@ -1,6 +1,27 @@
 export type GameContextValue =
   | undefined
   | {
-    value: { },
-    setValue: (value: object) => void
+    value: {};
+    setValue: ({ value, id }: { value: PaintingState;  id: number}) => void;
+    getPaintingState: (id: number) => PaintingState;
+    updateGameStatus: (value: PaintingState) => GameStatus;
+    updateCurrentStep: (value: PaintingState) => void
   }
+
+export type PaintingState = {
+  successStep: null | number;
+  failedStep: null | number;
+}
+
+export type GameState =
+  | {}
+  | {
+    [key: number]: PaintingState;
+  }
+
+export type SuccessStep = null | number;
+export type FailedStep = null | number;
+
+export type GameStatus = "success" | "ongoing" | "failed"
+
+export type GameStep = number;
--- src/app/context/gameProvider.tsx
+++ src/app/context/gameProvider.tsx
@@ -1,28 +1,65 @@
 'use client'
 
 import { createContext, useEffect, useState } from 'react';
-import { GameContextValue } from './gameContext.types';
+import { GameContextValue, GameState, GameStatus, GameStep, PaintingState } from './gameContext.types';
 
 export const GameContext = createContext<GameContextValue>(undefined)
 
 const GameProvider = ({ children }: { children: React.ReactNode }) => {
-  const [value, setValue] = useState({});
+  const [gameState, setGameState] = useState<GameState>({});
+  const [gameStatus, setGameStatus] = useState<GameStatus>('ongoing');
+  const [currentStep, setCurrentStep] = useState<GameStep>(1);
 
   useEffect(() => {
     const GTPData = localStorage.getItem("gameState")
-    if (GTPData) {
-      setValue(JSON.parse(GTPData))
-    }
+    if (GTPData) setGameState(JSON.parse(GTPData))
   }, [])
 
-  const handleSetValue = (value: object) => {
-    setValue(value)
-    localStorage.setItem("gameState", JSON.stringify(value))
+  const setPaintingState = ({ value, id }: { value: PaintingState; id: number }) => {
+    const newGameState = {
+      ...gameState,
+      [id]: value,
+    }
+    localStorage.setItem("gameState", JSON.stringify(newGameState))
+    setGameState(newGameState)
+  }
+
+  const getPaintingState = (id: number) => {
+    return gameState[id as keyof GameState]
+  }
+
+  const updateGameStatus = (value: PaintingState): GameStatus => {
+    if (value.successStep) {
+      return "success"
+    } else if (value.failedStep === 5) {
+      return "failed"
+    } else {
+      return "ongoing"
+    }
+  }
+
+  const updateCurrentStep = (value: PaintingState) => {
+    const successStep = value.successStep;
+    const failedStep = value.failedStep;
+
+    if (successStep === null && failedStep === null) {
+      setCurrentStep(1)
+    } else if (successStep === null && failedStep !== null) {
+      setCurrentStep(Math.min(5, failedStep + 1))
+    } else if (successStep !== null) {
+      setCurrentStep(successStep)
+    }
   }
 
   return (
     <GameContext.Provider
-      value={{value, setValue: handleSetValue}}
+      value={{
+        value: gameState,
+        setValue: setPaintingState,
+        getPaintingState,
+        updateGameStatus,
+        updateCurrentStep,
+      }}
     >
       {children}
     </GameContext.Provider>

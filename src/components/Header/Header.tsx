8e3658cf0439ebdc27cb3e9487f125de62e3ac4b
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-06-13 17:55:23 +0300

    navigate page title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import { InfoBox } from "./components/InfoBox";
 import { QuestionBox } from "./components/QuestionBox";
 
 const Header = () => {
   return (
     <div className="max-w-600 flex h-fit w-full items-center justify-center gap-2 px-1">
-      <h1 className="text-brown-light mr-auto self-start font-fairplay text-xl font-medium">
-        Guess the Painting
-      </h1>
+      <Link href="/" className="group mr-auto">
+        <h1 className="font-fairplay text-xl font-medium text-brown-light group-hover:text-blue-dark">
+          Guess the Painting
+        </h1>
+      </Link>
       <InfoBox />
       <QuestionBox />
     </div>

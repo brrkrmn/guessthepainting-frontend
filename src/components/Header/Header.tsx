d653409741e4f3b6d1bda70c2ec2777d000717ce
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-06-16 17:03:15 +0300

    add favicon and logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { InfoBox } from "./components/InfoBox";
 import { QuestionBox } from "./components/QuestionBox";
@@ -5,7 +6,16 @@ import { QuestionBox } from "./components/QuestionBox";
 const Header = () => {
   return (
     <div className="max-w-600 flex h-fit w-full items-center justify-center gap-2 px-1">
-      <Link href="/" className="group mr-auto">
+      <Link
+        href="/"
+        className="group mr-auto flex items-center justify-center gap-1"
+      >
+        <Image
+          src="/icons8-dailyart.png"
+          alt="Mona Lisa Icon"
+          width={18}
+          height={18}
+        />
         <h1 className="font-fairplay text-xl font-medium text-brown-light group-hover:text-blue-dark">
           Guess the Painting
         </h1>
